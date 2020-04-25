6f98bea7cfaccec07a9e2cd642aa2b895587958e
ozawako1 <ozawako1@gmail.com>
2020-04-25 17:04:22 +0900

    6th

--- GetMeetings/index.js
+++ GetMeetings/index.js
@@ -29,56 +29,56 @@ module.exports = function (context, req) {
             "body": { "Error": "Invalid Parameters." }
         };
         context.done();
-    }
+    } else {
+            // in app settings.
+        const ZOOM_API_KEY = process.env.ZOOM_API_KEY;
+        const ZOOM_API_SEC = process.env.ZOOM_API_SEC;
+        
+        var payload = {
+            iss: ZOOM_API_KEY,
+            exp: ((new Date()).getTime() + 5000)
+        }
+        var token = jwt.sign(payload, ZOOM_API_SEC);
+        
+        //Make Zoom API call
+        var options = {
+            uri: 'https://api.zoom.us/v2/users/' + zoom_user_id +'/meetings',
+            qs: {
+                type: 'upcoming', // -> uri + '?status=active'
+                page_size: 3
+            },
+            auth: {
+                //Provide your token here
+                'bearer': token
+            },
+            headers: {
+                'User-Agent': 'Zoom-Jwt-Request',
+                'content-type': 'application/json'
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
 
-    // in app settings.
-    const ZOOM_API_KEY = process.env.ZOOM_API_KEY;
-    const ZOOM_API_SEC = process.env.ZOOM_API_SEC;
-    
-    var payload = {
-        iss: ZOOM_API_KEY,
-        exp: ((new Date()).getTime() + 5000)
+        rp(options)
+            .then(function (response) {
+                //logic for your response
+                context.log('User has', response);
+                var users = JSON.stringify(response.meetings);
+                context.res = {
+                    "status": 200,
+                    "content-type": "application/json",
+                    "body": users
+                };
+                context.done();
+            })
+            .catch(function (err) {
+                context.log('API call failed, reason ', err);
+                context.res = {
+                    "status": 500,
+                    "content-type": "application/json",
+                    "body": { "Error": err.message + "|" + err.stack }
+                };
+                context.done();
+            });
     }
-    var token = jwt.sign(payload, ZOOM_API_SEC);
-    
-    //Make Zoom API call
-    var options = {
-        uri: 'https://api.zoom.us/v2/users/' + uid +'/meetings',
-        qs: {
-            type: 'upcoming', // -> uri + '?status=active'
-            page_size: 3
-        },
-        auth: {
-            //Provide your token here
-            'bearer': token
-        },
-        headers: {
-            'User-Agent': 'Zoom-Jwt-Request',
-            'content-type': 'application/json'
-        },
-        json: true // Automatically parses the JSON string in the response
-    };
-
-    rp(options)
-        .then(function (response) {
-            //logic for your response
-            context.log('User has', response);
-            var users = JSON.stringify(response.meetings);
-            context.res = {
-                "status": 200,
-                "content-type": "application/json",
-                "body": users
-            };
-            context.done();
-        })
-        .catch(function (err) {
-            context.log('API call failed, reason ', err);
-            context.res = {
-                "status": 500,
-                "content-type": "application/json",
-                "body": { "Error": err.message + "|" + err.stack }
-            };
-            context.done();
-        });
 
 };
